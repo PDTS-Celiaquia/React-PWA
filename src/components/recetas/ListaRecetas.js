a6f032b63eab060ed08a32275a4caa84e884025a
Leopoldo Lening Celaya <57147656+pololeningcelaya@users.noreply.github.com>
2021-08-16 15:00:17 -0300

    Get and list recetas

--- src/components/recetas/ListaRecetas.js
+++ src/components/recetas/ListaRecetas.js
@@ -1,7 +1,12 @@
-import { Container, withStyles } from "@material-ui/core";
+import {
+    Button, Container, IconButton, InputAdornment, TextField, Typography, withStyles
+} from '@material-ui/core'
 import { connect } from 'react-redux'
 import { getRecetas } from '../../store/actions'
 import React, { Component } from 'react';
+import Loader from '../common/Loader';
+import SearchIcon from '@material-ui/icons/Search';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 const styles = theme => ({
     title: {
@@ -9,6 +14,13 @@ const styles = theme => ({
     },
     container: {
         marginTop: theme.spacing(2)
+    },
+    header: {
+        margin: theme.spacing(1)
+    },
+    refresh: {
+        float: 'right',
+        color: theme.palette.text.primary,
     },
     element: {
         marginTop: theme.spacing(2),
@@ -20,12 +32,60 @@ class ListaRecetas extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            filter: "",
+        }
+
+        this.onFilterChange = this.onFilterChange.bind(this)
     }
 
+    componentDidMount() {
+        this.props.getRecetas()
+    }
+    
+    onFilterChange(e) {
+        const { value } = e.target
+        this.setState({ filter: value })
+    }
+    
     render() {
-        const { classes } = this.props;
+        const { filter } = this.state
+        const { recetas, fetching, classes } = this.props
+        if (!recetas && !fetching) {
+            return <p>Error</p>
+        }
+        const re = filter ? new RegExp(filter, 'ig') : null
+        const filteredList = re != null ? recetas.filter(receta => receta.nombre.match(re)) : recetas
         return (
             <Container className={classes.container} maxWidth="xs">
+                <div className={classes.header} maxWidth="xs">
+                    <TextField
+                        value={filter}
+                        onChange={this.onFilterChange}
+                        variant="outlined"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <IconButton
+                        className={classes.refresh}
+                        onClick={this.refresh}
+                    >
+                        <RefreshIcon />
+                    </IconButton>
+                </div>
+                
+                {fetching ? <Loader /> :
+                    <>
+                        {filteredList.map(receta => (
+                            <Typography>{receta.nombre}</Typography>
+                        ))}
+                    </>
+                }
 
             </Container>
 
