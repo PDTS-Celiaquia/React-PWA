aa060d29a110ca90bb25d28bf3663baf7c9996f2
Leopoldo Lening Celaya <57147656+pololeningcelaya@users.noreply.github.com>
2021-08-16 14:28:24 -0300

    Redux store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,61 @@
-const initialState = {
+import { typeDefs } from './actions';
 
+const {
+    requestUnidades, successUnidades, errorUnidades,
+    requestAlimentos, successAlimentos, errorAlimentos,
+    requestRecetas, successRecetas, errorRecetas,
+} = typeDefs
+
+const initState = {
+    alimentos: null,
+    fetchingAlimentos: false,
+    errorAlimentos: false,
+
+    recetas: null,
+    fetchingRecetas: false,
+    errorRecetas: false,
+
+    unidadesDeMedida: null,
+    fetchingUnidades: false,
+    errorUnidades: false,
+
+    sendingAlimento: false,
+    errorSendAlimento: null
 }
 
-const reducer = (state = initialState, { type, payload }) => {
+export default function reducer(state = initState, { type, payload, error }) {
     switch (type) {
 
-    default:
-        return state
+        case requestAlimentos:
+            return { ...state, fetchingAlimentos: true, errorAlimentos: false }
+
+        case successAlimentos:
+            return { ...state, fetchingAlimentos: false, alimentos: payload }
+
+        case errorAlimentos:
+            return { ...state, fetchingAlimentos: false, errorAlimentos: true, message: error }
+
+        case requestUnidades:
+            return { ...state, fetchingUnidades: true, errorUnidades: false }
+
+        case successUnidades:
+            return { ...state, fetchingUnidades: false, unidadesDeMedida: payload }
+
+        case errorUnidades:
+            return { ...state, fetchingUnidades: false, errorUnidades: true, message: error }
+
+
+        case requestRecetas:
+            return { ...state, fetchingRecetas: true, errorRecetas: false }
+
+        case successRecetas:
+            return { ...state, fetchingRecetas: false, recetas: payload }
+
+        case errorRecetas:
+            return { ...state, fetchingRecetas: false, errorRecetas: true, message: error }
+
+
+        default:
+            return state
     }
 }
-
-export default reducer;
